0b0ad36b0b2bf6b74c4b5ff375705044dc2799e2
THCManarin <76226437+THCManarin@users.noreply.github.com>
2022-07-07 22:40:37 -0300

    GRAPH TEST 1

--- projects/EnzyView/main.js
+++ projects/EnzyView/main.js
@@ -1,5 +1,73 @@
-import {Reaction} from './modules/Reaction.js'
+// import {Reaction} from './modules/Reaction.js'
 
-const test1 = new Reaction
 
-console.log(test1.rate(1))
+class Reaction
+{
+    rate(parameters)
+    {
+        return 0;
+    }
+}
+
+class MichaelisMenten extends Reaction
+{
+    rate(parameters)
+    {
+        const vMax = parameters.vMax
+        const substrate = parameters.substrate
+        const km = parameters.km
+
+        return vMax * substrate / (km + substrate)
+    }
+}
+
+class parameters
+{
+    vMax
+    substrate
+    km    
+}
+
+function executa() {
+
+	event.preventDefault();
+    let param = new parameters
+    param.vMax = document.querySelector('#vmax').value
+    param.substrate = 1
+    param.km = 1    
+
+    const test1 = new MichaelisMenten
+
+    document.querySelector('#resultado').value = test1.rate(param)
+}
+document.querySelector('#button').addEventListener('click', executa);
+
+const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+  const myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
